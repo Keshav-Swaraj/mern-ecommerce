812ab430021f8fa1286024e254455ce475da6f22
keshav <keshavswaraj75@gmail.com>
2025-07-31 23:16:54 +0530

    login status code update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
                 role: user.role
             });
         }else{
-            return res.status(401).json({ message: "Invalid email or password" });
+            return res.status(400).json({ message: "Invalid email or password" });
         }
 
     } catch (error) {
